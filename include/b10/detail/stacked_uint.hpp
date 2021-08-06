56e1ee4fadb860670aa76f65f452f2c2fcd3a347
Yuri Kilochek <yuri.kilochek@gmail.com>
2021-08-06 20:11:45 +0300

    rename double_width_uint -> stacked_uint and add selecting uint

---
+++ include/b10/detail/stacked_uint.hpp
@@ -0,0 +1,164 @@
+#ifndef B10_INCLUDED_DETAIL_STACKED_UINT_HPP
+#define B10_INCLUDED_DETAIL_STACKED_UINT_HPP
+
+#include <b10/detail/width_of.hpp>
+
+#include <b10/detail/thirdparty/hedley.h>
+
+#include <bit>
+#include <concepts>
+
+namespace b10::detail {
+namespace stacked_uint_detail {
+
+template <typename Half, std::endian = std::endian::native>
+struct storage;
+
+template <typename Half>
+struct storage<Half, std::endian::little> {
+    Half lo;
+    Half hi;
+};
+
+template <typename Half>
+struct storage<Half, std::endian::big> {
+    Half hi;
+    Half lo;
+};
+
+} // namespace stacked_uint_detail
+
+template <typename Half>
+struct stacked_uint
+: stacked_uint_detail::storage<Half>
+{
+    stacked_uint()
+    = default;
+
+    template <typename T>
+    HEDLEY_ALWAYS_INLINE constexpr
+    stacked_uint(T x)
+    requires { *this = x; }
+    { *this = x; }
+
+    template <typename OtherHalf>
+    HEDLEY_ALWAYS_INLINE constexpr
+    auto operator=(stacked_uint<OtherHalf> x)
+    -> stacked_uint<Half>&
+    {
+        if constexpr(width_of<OtherHalf> < width_of<Half>) {
+            lo = x;
+            hi = 0;
+        } else {
+            *this = x.lo;
+        }
+        return *this;
+    }
+
+    template <std::convertible_to<Half> T>
+    HEDLEY_ALWAYS_INLINE constexpr
+    auto operator=(T x)
+    -> stacked_uint<Width>&
+    {
+        lo = x;
+        hi = -(x < 0);
+        return *this;
+    }
+
+    template <typename T>
+    requires std::convertible_to<Half, T>
+    HEDLEY_ALWAYS_INLINE constexpr
+    operator T() const {
+        return lo;
+    }
+};
+
+template <typename Half>
+HEDLEY_ALWAYS_INLINE constexpr
+auto operator<=>(stacked_uint<Half> x, stacked_uint<Half> y)
+-> std::strong_ordering
+{
+    if (const auto ordering = x.hi <=> y.hi; ordering != 0)
+        return ordering;
+
+    return x.lo <=> y.lo;
+}
+
+template <typename Half>
+HEDLEY_ALWAYS_INLINE constexpr
+auto operator~(stacked_uint<Half> x)
+-> stacked_uint<Half>
+{
+    stacked_uint<Half> r;
+
+    r.lo = ~x.lo;
+    r.hi = ~x.hi;
+
+    return r;
+}
+
+template <typename Half>
+HEDLEY_ALWAYS_INLINE constexpr
+auto operator&(stacked_uint<Half> x, stacked_uint<Half> y)
+-> stacked_uint<Half>
+{
+    stacked_uint<Half> r;
+
+    r.lo = x.lo & y.lo;
+    r.hi = x.hi & y.hi;
+
+    return r;
+}
+
+template <typename Half>
+HEDLEY_ALWAYS_INLINE constexpr
+auto operator|(stacked_uint<Half> x, stacked_uint<Half> y)
+-> stacked_uint<Half>
+{
+    stacked_uint<Half> r;
+
+    r.lo = x.lo | y.lo;
+    r.hi = x.hi | y.hi;
+
+    return r;
+}
+
+template <typename Half>
+HEDLEY_ALWAYS_INLINE constexpr
+auto operator<<(stacked_uint<Half> x, std::size_t y)
+-> stacked_uint<Half>
+{
+    stacked_uint<Half> r;
+
+    if (y < width_of<Half>) {
+        r.lo = x.lo << y;
+        r.hi = (x.hi << y) | (x.lo >> (width_of<Half> - y));
+    } else {
+        r.lo = 0;
+        r.hi = x.lo << (y - width_of<Half>);
+    }
+
+    return r;
+}
+
+template <typename Half>
+HEDLEY_ALWAYS_INLINE constexpr
+auto operator>>(stacked_uint<Half> x, std::size_t y)
+-> stacked_uint<Half>
+{
+    stacked_uint<Half> r;
+
+    if (y < width_of<Half>) {
+        r.lo = (r.hi << (width_of<Half> - y)) | (x.lo >> y);
+        r.hi = x.hi >> y;
+    } else {
+        r.lo = r.hi >> (y - width_of<Half>);
+        r.hi = 0;
+    }
+
+    return r;
+}
+
+} // namespace b10::detail
+
+#endif
---
+++ include/b10/detail/uint.hpp
@@ -0,0 +1,36 @@
+#ifndef B10_INCLUDED_DETAIL_UINT_HPP
+#define B10_INCLUDED_DETAIL_UINT_HPP
+
+#include <b10/detail/builtin_uint.hpp>
+#include <b10/detail/stacked_uint.hpp>
+
+#include <cstddef>
+
+namespace b10::detail {
+namespace uint_detail {
+
+template <std::size_t Width>
+struct select;
+
+} // namespace uint_detail
+
+template <std::size_t Width>
+using uint = uint_detail::select<Width>::type;
+
+namespace uint_detail {
+
+template <std::size_t Width>
+requires requires { typename builtin_uint<Width>; }
+struct select<Width> {
+    using type = builtin_uint<Width>;
+};
+
+template <std::size_t Width>
+struct select {
+    using type = stacked_uint<uint<Width / 2>>;
+};
+
+} // namespace uint_detail
+} // namespace b10::detail
+
+#endif
